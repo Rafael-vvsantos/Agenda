a5a3bbebb046d2bcc9a5b0900eae62827f432710
Rafael Santos <rafasantos321.rs58@gmail.com>
2021-03-18 10:05:17 -0300

    Implementação da função listar e alterações no codigo principal

--- Agenda.C
+++ Agenda.C
@@ -3,19 +3,22 @@
 #include <string.h>
 
 #define Tamanho_Vetor_Pessoa 10 //definindo o tamanho ddo vetor
-typedef struct Pessoa          //criando a lista e já definindo novo dado como agenda
+typedef struct Pessoa           //criando a lista e já definindo novo dado como agenda
 {
     int codigo;
     char nome_completo[50];
-    char telefone[11];
-    char email[30];
+    char telefone[15];
+    char email[40];
     char data_de_nascimento[10];
-    char obs[25];
+    char obs[100];
 } Pessoa;
 
 void menu(); //menu feito
 void cadastro();
+void listar();
 
+Pessoa contato[Tamanho_Vetor_Pessoa];
+int numero_de_contatos = 0, i;
 int main(void) // programa principal a ser trabalhado contendo while e switch
 {
 
@@ -27,11 +30,16 @@ int main(void) // programa principal a ser trabalhado contendo while e switch
         menu();
         printf("\n\nEscolha uma opcao:");
         scanf("%d", &opcao);
+        system("cls");
 
         switch (opcao)
         {
         case 1:
             cadastro();
+            break;
+
+        case 2:
+            listar();
             break;
         }
     }
@@ -39,8 +47,10 @@ int main(void) // programa principal a ser trabalhado contendo while e switch
 
 void menu()
 {
-    printf("                             *AGENDA*                                            \n");
-    printf("__________________________________________________________________ ______________\n\n");
+    printf(" ________________________________________________________________________________\n");
+    printf("|                                                                                |\n");
+    printf("|                               AGENDA                                           |\n");
+    printf("|________________________________________________________________________________|\n\n");
     printf(">>>>>>>>>>> MENU <<<<<<<<\n\n");
     printf("1 - Cadastro\n");
     printf("2 - Listar\n");
@@ -51,11 +61,10 @@ void menu()
     printf(">>>>>>>>>>>>>><<<<<<<<<<<<\n");
 }
 
-void cadastro() //Cadastramentos do dos nomes, zerando todas a posições e seguindo para guardas os dados 
+void cadastro() //Cadastramentos do dos nomes, zerando todas a posições e seguindo para guardas os dados
 {
 
-    Pessoa contato[Tamanho_Vetor_Pessoa];
-    int i, op = 1;
+    int op = 1;
 
     for (i = 0; i < Tamanho_Vetor_Pessoa; i++)
     {
@@ -90,6 +99,8 @@ void cadastro() //Cadastramentos do dos nomes, zerando todas a posições e segu
 
             printf("Digite a observacao:");
             scanf("%s", contato[i].obs);
+
+            numero_de_contatos = numero_de_contatos + 1;
         }
         else
         {
@@ -98,4 +109,38 @@ void cadastro() //Cadastramentos do dos nomes, zerando todas a posições e segu
         printf("Pressione 0 para retornar ao menu:\n"); //* pedir para o usuraio retornar
         scanf("%i", &op);
     }
+}
+
+void listar()
+{
+    int continua;
+    printf(" ________________________________________________________________________________\n");
+    printf("|                                                                                |\n");
+    printf("|                              CONTATOS                                          |\n");
+    printf("|________________________________________________________________________________|\n\n");
+
+    if (numero_de_contatos > 0)
+    {
+        for (i = 0; i < numero_de_contatos; i++)
+        {
+
+            printf("Codigo : %i\n", contato[i].codigo);
+
+            printf("Nome Completo : %s\n", contato[i].nome_completo);
+
+            printf("Telefone : %s\n", contato[i].telefone);
+
+            printf("Email : %s\n", contato[i].email);
+
+            printf("Data de Nascimento :%s\n", contato[i].data_de_nascimento);
+
+            printf("Obervacoes : %s\n", contato[i].obs);
+        }
+    }
+    else
+    {
+        printf("Agenda Vazia!\n\n");
+    }
+    printf("Pressione 0 para retornar ao menu:\n"); //* pedir para o usuraio retornar
+    scanf("%i", &continua);
 }
